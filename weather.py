06c26ec40954d1a29a6329f359dd48c3ce484021
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2023-03-25 05:32:32 -0400

    first commit

---
+++ weather.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+if __name__ == '__main__':
+    print("WEATHER FORECASTER, FOLLOW THE FOLLOWING INSTRUCTIONS AND GET THE WEATHER")
+    print("*"*50)
+    print("Input city")
+    print("Input country")
+
+
