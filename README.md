e29e80c2d4ca4a22e9b9e584a23ebbdf40b07388
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2023-03-25 22:39:57 -0400

    finished script, README and added requirements.txt

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-### Writes an email and sends it to you if it is possible that it will rain today
+# Bad weather notifier
+Made for the people that leave their house in the morning without an umbrella when it will rain all day long because they didn't check the weather (me) !
 
-- Need to put your own api credentials in a file called "api-key.txt" for https://home.openweathermap.org/, 
-under the same directory as the python file
+## How to set up
+- You will need to put your own api credentials in a file called "api-keys.json" for [Open Weather](https://home.openweathermap.org/), 
+ and for [SendGrid](https://sendgrid.com/) as well as your sender and target emails under the same directory as the python file, that file should be a json file with
+ the following properties:
+  - "OpenWeatherKey" for the open weather api key
+  - "SendGridKey" for the send grid api key, and
+  - "SendGridSender" for the email of the sender
+    - For these 2 you need to make and set up a SendGrid account, create a new sender (whose email you will set as the value of the SendGridSender property) and make a new API key with full access then set the value SendGridKey to be that API key 
+  - "TargetEmail" for the target email
 
-- Also need to run json-retrieve.sh under bash-scripts
+- You will need to run json-retrieve.sh under bash-scripts to get the required Json files that are needed to run the script
 
-- ISO 3166 retrieved from https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes
+- Finally, you will need to download the lybraries using ```pip install -r requirements.txt```, ideally you would do so in a virtual environment to not mess with your global lybraries
 
-- Coutry list retrieved from https://github.com/russ666/all-countries-and-cities-json.git
+- Once all of that is done, you can run the python script
+
+- Use Windows task scheduler and such to automatically run it once a day
+
+## How to use
+### CLI
+- You can run the script through the shell this way ```python3 .\weather.py [city-name] [country-alpha-3]```
+  - citty-name needs to be capitalized
+### IO
+ - You can also just run the script directly and input the required information on the go
+   - You will first be prompted to input the city name
+   - You will then be prompted to input the country name - 
+#### In both cases the TargetEmail in the .json you made will receive an email from the SendGridSender email if the weather isn't too good.
+
+## Sources
+- ISO 3166 json retrieved from [lukes / ISO-3166-Countries-with-Regional-Codes ](https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes)
+
--- weather.py
+++ weather.py
@@ -1,30 +1,37 @@
 import requests
 import sys
 import json
-import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import *
+import time
+
 
 def setup():
     """
     Sets up global variables
-    :return: None
     """
-    global COUNTRY_TO_ISO, COUNTRY_TO_CITY, API_CREDS
+    global WEATHER_API_KEY, EMAIL_API_KEY, SENDER, ALPHA3_TO_ISO, TARGET_EMAIL, TODAY
 
-    # This program uses https://openweathermap.org/ for requests, need to set your own api key and
-    # write api key info in a file called api-key.txt
-    with open("api-key.txt", "r") as creds:
-        API_CREDS = creds.read()
+    TODAY = time.localtime()
+
+    # This program uses https://openweathermap.org/ for weather requests,
+    # and sendgrip for email sending, need to set your own api keys and other important values
+    # in a file called api-keys.json, refer to ./README.md for more details
+    with open("api-keys.json", "r") as creds:
+        json_file = creds.read()
+        api_obj = json.loads(json_file)
+        WEATHER_API_KEY = api_obj["OpenWeatherKey"]
+        EMAIL_API_KEY = api_obj["SendGridKey"]
+        SENDER = api_obj["SendGridSender"]
+        TARGET_EMAIL = api_obj["TargetEmail"]
+
     # gets Country to ISO mapping
     with open("data/iso-3166-code.json") as iso:
         json_file_content = iso.read()
         raw_json = json.loads(json_file_content)
-        COUNTRY_TO_ISO = {}
+        ALPHA3_TO_ISO = {}
         for att in raw_json:
-            COUNTRY_TO_ISO[att["name"]] = att["country-code"]
-    # gets list of cities for each country
-    with open("data/cities.json") as cities:
-        content = cities.read()
-        COUNTRY_TO_CITY = json.loads(content)
+            ALPHA3_TO_ISO[att["alpha-3"]] = att["name"]
 
 
 def prompt():
@@ -37,39 +44,85 @@ def prompt():
     print("Please input the following information with the first letter of every word upperCased,"
           "and only using ascii characters")
     city = input("Input City : ")
-    country = input("Input country : ")
+    country = input("Input country's alpha 3 : ")
     return (city, country)
 
 
-def get_lat_lon(city, country):
+def get_weather_information(city, country):
     """
-    Fetches the city, country's latitude and longitude.
-    :param city: city whose latitude, longitude we want
-    :param country: the country of that city
-    :return: a tuple of the latitude and longitude
+    Fetches the city, country's weather information.
+    :param city: city whose weather we want to fetch
+    :param country: the country of that city in alpha-3 form
+    :return: https://openweathermap.org/ response to request.
     """
-    country_iso = COUNTRY_TO_ISO[country]
-    response = requests\
-        .get(f"https://api.openweathermap.org/data/2.5/weather?"
-             f"q={city},{country_iso}&appid={API_CREDS}&units=metric")
+    country_iso = ALPHA3_TO_ISO[country]
+    try:
+        response = requests \
+            .get(f"https://api.openweathermap.org/data/2.5/weather?"
+                 f"q={city},{country_iso}&appid={WEATHER_API_KEY}&units=metric", timeout=5)
+    except requests.exceptions.Timeout:
+        exit(f"Request Time out.")
+
+    if not response.ok:
+        exit(f"Request failed, Response : \n{response}\n{response.content}")
+
     api_response = response.json()
-    print(api_response)
-    lat = api_response["coord"]["lat"]
-    lon = api_response["coord"]["lon"]
-    return lat, lon
+
+    return api_response
+
+
+def bad_weather(response):
+    """
+    Checks if the weather is misty, if it will snow, rain or if there will be a storm
+    :param response: https://openweathermap.org/ response to weather request.
+    :return: true if to expect bad weather, false otherwise.
+    """
+    weather_id_code = int(response["weather"][0]["id"] / 100)
+    return weather_id_code == 2 or weather_id_code == 5 or weather_id_code == 6 or weather_id_code == 7
+
+
+def send_alert_email(description, temp, humidity):
+    """
+    Sends an email with the appropriate description, temperature and humidty to TARGET_EMAIL, from SENDER
+    :param description: the description of the weather
+    :param temp: temperature (in Celsius)
+    :param humidity: humidity
+    """
+    # email stuff
+    body = f"The weather today isn't too good, expect {description} with an \n" \
+           f"average temperature of {temp}°C and {humidity}% humidity \n" \
+           f"(information may not be perfect or 100% accurate)"
+    message = Mail(from_email=SENDER, to_emails=TARGET_EMAIL,
+                   subject=f"Bad weather alert {TODAY.tm_mday}/{TODAY.tm_mon}/{TODAY.tm_year}",
+                   plain_text_content=body)
+    sg = SendGridAPIClient(EMAIL_API_KEY)
+    sg.send(message)
 
 
 if __name__ == '__main__':
     setup()
-    city, country = prompt()
+    arguments = sys.argv
+    if len(arguments) == 3:
+        city = arguments[1]
+        country = arguments[2]
+    else:
+        # IO
+        city, country = prompt()
+
+    # Alpha 3 is all upper case in the json
+    country = country.upper()
 
     try:
-        COUNTRY_TO_ISO[country]
+        ALPHA3_TO_ISO[country]
     except KeyError:
         exit("Country invalid")
 
-    if city not in COUNTRY_TO_CITY[country]:
-        exit("City invalid")
+    response = get_weather_information(city, country)
+    if bad_weather(response):
+        print("Sending bad weather email...")
+        description = response['weather'][0]['description']
+        temp = response['main']['temp']
+        humidity = response['main']['humidity']
+        send_alert_email(description, temp, humidity)
 
-    lat, lon = get_lat_lon(city, country)
 
